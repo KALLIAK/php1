f8e066f13017928e22678e64adaab11173e5d86d
Victor <artoboi.by@gmail.com>
2018-08-04 21:36:05 +0300

    Bootstrap added

--- day2/auth/login.php
+++ day2/auth/login.php
@@ -16,11 +16,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 
 ?>
-<form method="post">
-    <label for="name">Логин:</label><br>
-    <input id="name" name="login"> <br>
-    <label for="pass">Пароль:</label><br>
-    <input type="password" id="pass" name="password"><br>
-    <input type="checkbox" id="remember" name="remember">Запомнить<br>
-    <input type="submit" value="Авторизация">
-</form>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Login form</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+<div class="container">
+    <form method="post">
+        <label for="name">Логин:</label><br>
+        <input id="name" name="login"> <br>
+        <label for="pass">Пароль:</label><br>
+        <input type="password" id="pass" name="password"><br>
+        <input type="checkbox" id="remember" name="remember">Запомнить<br>
+        <input type="submit" value="Авторизация">
+    </form>
+</div>
+</body>
--- day2/auth/main.php
+++ day2/auth/main.php
@@ -1,10 +1,11 @@
 <?php
 session_start();
+
 if (isset($_COOKIE['login'])
     && $_COOKIE['login'] == 'f9a81477552594c79f2abc3fc099daa896a6e3a3590a55ffa392b6000412e80b'
     && isset($_COOKIE['password'])
     && $_COOKIE['password'] == 'af838d6547c4ca7f4c5247320d0910e4c04da5d21eaccdb831ab31169b9005a1'
-    ) {
+) {
     $_SESSION['is_auth'] = true;
 }
 
@@ -15,5 +16,20 @@ if (!isset($_SESSION['is_auth']) || !$_SESSION['is_auth']) {
 
 echo 'Closed info';
 ?>
-<br>
-<a href="logout.php">Выход</a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Main page</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+    <br>
+    <button><a href="logout.php" style="text-decoration: none">Выход</a></button>
+</div>
+</body>
