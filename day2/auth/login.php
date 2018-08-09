1da6675bdb16fc02d897dba15e4ec4d11c4f7946
Victor Shvid <artoboi.by@gmail.com>
2018-08-09 10:04:47 +0300

    fix

--- day2/auth/login.php
+++ day2/auth/login.php
@@ -4,9 +4,9 @@ session_start();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($_POST['login'] == 'admin' && $_POST['password'] == '12345') {
         $_SESSION['is_auth'] = true;
-        if (isset($_POST['remember']) && $_POST['remember']) {
-            setcookie('login', hash('sha256', 'admin' . 'salt'), time() + 3600 * 24 * 7);
-            setcookie('password', hash('sha256', '12345' . 'salt'), time() + 3600 * 24 * 7);
+        if (isset($_POST['remember'])) {
+            setcookie('login', hash('sha256', 'admin' . 'salt'), time() + 3600 * 24 * 7, '/');
+            setcookie('password', hash('sha256', '12345' . 'salt'), time() + 3600 * 24 * 7, '/');
         }
         header('Location: main.php');
         exit();
