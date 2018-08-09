a2bf11737f82edfd3ffe91700537613fecce7db1
Victor Shvid <artoboi.by@gmail.com>
2018-08-09 10:05:01 +0300

    path fix

--- day2/auth/logout.php
+++ day2/auth/logout.php
@@ -6,12 +6,12 @@ if (isset($_SESSION['is_auth'])) {
 
 if (isset($_COOKIE['login'])) {
     unset($_COOKIE['login']);
-    setcookie('login', null, -1);
+    setcookie('login', null, -1,'/');
 }
 
 if (isset($_COOKIE['password'])) {
     unset($_COOKIE['password']);
-    setcookie('password', null, -1);
+    setcookie('password', null, -1, '/');
 }
 
 header('Location: login.php');
