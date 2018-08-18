e92e1c66480e839c595d5150acaf351913d88381
Victor <artoboi.by@gmail.com>
2018-08-18 18:57:50 +0300

    $isAuth

--- day1/4/edit.php
+++ day1/4/edit.php
@@ -2,18 +2,34 @@
 include_once 'functions.php';
 session_start();
 
-if (isset($_COOKIE['login'])
-    && $_COOKIE['login'] == 'f9a81477552594c79f2abc3fc099daa896a6e3a3590a55ffa392b6000412e80b'
-    && isset($_COOKIE['password'])
-    && $_COOKIE['password'] == 'af838d6547c4ca7f4c5247320d0910e4c04da5d21eaccdb831ab31169b9005a1'
-) {
-    $_SESSION['is_auth'] = true;
+$isAuth = false;
+
+if (isset($_SESSION['is_auth']) && $_SESSION['is_auth'] === true) {
+    $isAuth = true;
+} elseif (isset($_COOKIE['login']) && isset($_COOKIE['password'])) {
+    if ($_COOKIE['login'] == 'f9a81477552594c79f2abc3fc099daa896a6e3a3590a55ffa392b6000412e80b'
+    && $_COOKIE['password'] == 'af838d6547c4ca7f4c5247320d0910e4c04da5d21eaccdb831ab31169b9005a1') {
+        $isAuth = true;
+    }
 }
 
-if (!isset($_SESSION['is_auth']) || !$_SESSION['is_auth']) {
+if ($isAuth === false) {
     header('Location: login.php');
     exit();
 }
+
+//if (isset($_COOKIE['login'])
+//    && $_COOKIE['login'] == 'f9a81477552594c79f2abc3fc099daa896a6e3a3590a55ffa392b6000412e80b'
+//    && isset($_COOKIE['password'])
+//    && $_COOKIE['password'] == 'af838d6547c4ca7f4c5247320d0910e4c04da5d21eaccdb831ab31169b9005a1'
+//) {
+//    $_SESSION['is_auth'] = true;
+//}
+//
+//if (!isset($_SESSION['is_auth']) || !$_SESSION['is_auth']) {
+//    header('Location: login.php');
+//    exit();
+//}
 
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $title = htmlspecialchars($_GET['fname']) ?? null;
