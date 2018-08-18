e9304c408b1bd445c59b79b135a958c87356925d
Victor <artoboi.by@gmail.com>
2018-08-18 15:31:17 +0300

    styles.css

--- day1/4/login.php
+++ day1/4/login.php
@@ -23,6 +23,7 @@ $msg = $msg ?? '';
     <title>Login form</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="./css/styles.css"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
@@ -39,7 +40,7 @@ $msg = $msg ?? '';
         <input type="password" id="pass" name="password"><br>
         <input type="checkbox" id="remember" name="remember">Запомнить<br>
         <input type="submit" value="Авторизоваться">
-        <button><a href="index.php" style="text-decoration: none; color: black">На главную</a></button>
+        <button><a href="index.php">На главную</a></button>
     </form>
 </div>
 </body>
